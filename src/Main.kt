a57cba61aa128dce73efeb0c2d6f2ad9d630753b
Claudio Cesar Goncalves Dias Junior <claudio.c.junior@kroton.com.br>
2024-04-11 11:50:10 -0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+fun ex2w() {
+    var i = 1
+    while (i <= 50) {
+        if (i % 15 == 0) {
+            print("FizzBuzz ")
+        } else if (i % 3 == 0) {
+            print("Buzz ")
+        } else if (i % 5 == 0) {
+            print("Fizz ")
+        } else {
+            print("$i ")
+        }
+        i++
+
+    }
+}
+
+fun main() {
+    ex2w()
+}
